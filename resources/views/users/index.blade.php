8f45e6b5e9ddc662e3aae0023ff3aaa7adada085
adem <adembm91@gmail.com>
2023-02-08 15:47:49 +0100

    first commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app-master')
+
+@section('content')
+    <div class="bg-light p-4 rounded">
+        <h1>Users</h1>
+        <div class="lead">
+            Manage your users here.
+            <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm float-right">Add new user</a>
+        </div>
+
+        <div class="mt-2">
+            @include('layouts.partials.messages')
+        </div>
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col" width="1%">#</th>
+                <th scope="col" width="15%">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col" width="10%">Username</th>
+                <th scope="col" width="1%" colspan="3"></th>    
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->username }}</td>
+                        <td><a href="{{ route('users.show', $user->id) }}" class="btn btn-warning btn-sm">Show</a></td>
+                        <td><a href="{{ route('users.edit', $user->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                        <td>
+                        <form action="{{route('users.destroy',$user->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn-second" class="btn btn-warning btn-sm" style="color:white;background-color:red;border:none;padding: 0.25rem 0.5rem;font-size: .875rem;line-height: 1.5;border-radius: 0.2rem;">Delete</button>
+                            
+                        </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="d-flex">
+            {!! $users->links() !!}
+        </div>
+
+    </div>
+@endsection
